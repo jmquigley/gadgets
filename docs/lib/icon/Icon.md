b410cb06d1b235f4c6e0baa9a5d7f00c3f2661ce
James Quigley <jmquigley@outlook.com>
2017-06-09 09:01:24 -0500

    Fixes build/test bug in props.ts

---
+++ docs/lib/icon/Icon.md
@@ -0,0 +1,39 @@
+<a name="Icon"></a>
+
+## Icon
+Displays a graphical icon within the current container.  The control uses
+[Font Awesome](http://fontawesome.io/) for the icons.  It can also
+accept an image file.  It uses four sizings for the icons: small
+medium/normal, large, and xlarge.  These are exposed through an
+enumeration named `IconSize`.
+
+#### Examples:
+
+```javascript
+import {Icon, IconSize} from 'gadgets';
+<Icon size={IconSize.small} iconName="cab" />
+<Icon size={IconSize.normal} iconName="paper-plane-o" />
+<Icon imageFile="./image.png" />
+<Icon iconName="cab" color="red" backgroundColor="gray" />
+```
+
+#### Events
+None
+
+#### Styles
+- `ui-icon` - applied to the top level container for the icon.
+
+#### Properties
+- `backgroundColor: string` - Sets the background color of the icon when
+`iconName` is used.
+- `color: string` - Sets the icon color when `iconName` is used.  The
+default is *black*.
+- `iconName: string` - The name of the font awesome icon that will be
+used in this icon.  This option is mutually exclusive to imageFile
+- `imageFile: string` - The path to an image file that will be used in
+this icon.  This option is mutually excludive to iconName.
+- `size: IconSize` - There are four icon sizes that can be used:
+IconSize.small (16px), IconSize.medium (36px), IconSize.large (48px),
+IconSize.xlarge (64px).
+
+**Kind**: global class  
--- test/test_props.ts
+++ test/test_props.ts
@@ -11,7 +11,10 @@ test('Test retrieval of default prop object', t => {
 	const props = getDefaultBaseProps();
 
 	t.true('backgroundColor' in props);
-	t.is(props.backgroundColor, '');
+	t.is(props.backgroundColor, 'inherit');
+
+	t.true('borderColor' in props);
+	t.is(props.borderColor, 'inherit');
 
 	t.true('children' in props);
 	t.is(props.children, null);
@@ -23,7 +26,10 @@ test('Test retrieval of default prop object', t => {
 	t.is(props.classes.length, 0);
 
 	t.true('color' in props);
-	t.is(props.color, '');
+	t.is(props.color, 'inherit');
+
+	t.true('contentEditable' in props);
+	t.false(props.contentEditable);
 
 	t.true('disabled' in props);
 	t.false(props.disabled);
@@ -31,11 +37,35 @@ test('Test retrieval of default prop object', t => {
 	t.true('id' in props);
 	t.is(props.id, '');
 
+	t.true('noedit' in props);
+	t.false(props.noedit);
+
 	t.true('noripple' in props);
 	t.false(props.noripple);
 
+	t.true('onBlur' in props);
+	t.true(props.onClick != null);
+
+	t.true('onChange' in props);
+	t.true(props.onChange != null);
+
 	t.true('onClick' in props);
 	t.true(props.onClick != null);
+
+	t.true('onClose' in props);
+	t.true(props.onClose != null);
+
+	t.true('onDoubleClick' in props);
+	t.true(props.onDoubleClick != null);
+
+	t.true('onKeyPress' in props);
+	t.true(props.onKeyPress != null);
+
+	t.true('onKeyDown' in props);
+	t.true(props.onKeyDown != null);
+
+	t.true('onMouseOut' in props);
+	t.true(props.onMouseOut != null);
 
 	t.true('selected' in props);
 	t.false(props.selected);
