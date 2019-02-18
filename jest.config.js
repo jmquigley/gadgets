863c43ecbdab3a05e423007f1fe87818f4f91c2c
James Quigley <jmquigley@outlook.com>
2019-02-18 15:29:08 -0600

    Updates dependencies

---
+++ jest.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+	collectCoverage: true,
+	coveragePathIgnorePatterns: [
+		"<rootDir>/bin/",
+		"<rootDir>/__tests__/helpers",
+		"<rootDir>/node_modules"
+	],
+	moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json"],
+	moduleNameMapper: {
+		"\\.(css|style)$": "identity-obj-proxy"
+	},
+	notify: false,
+	setupFiles: [
+		"<rootDir>/bin/workaround.js",
+		"<rootDir>/bin/setupEnzyme.js",
+		"<rootDir>/bin/setupDOM.js"
+	],
+	setupFilesAfterEnv: ["<rootDir>/bin/setupJest.js"],
+	snapshotSerializers: ["enzyme-to-json/serializer"],
+	testPathIgnorePatterns: [
+		"<rootDir>/bin/",
+		"<rootDir>/dist",
+		"<rootDir>/__tests__/helpers",
+		"<rootDir>/node_modules"
+	],
+	verbose: true
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,30 @@
-const {leader} = require('util.leader');
+const {leader} = require("util.leader");
 
-const TsDeclarationBundlerPlugin = require('ts-declaration-webpack-plugin');
-const CircularDependencyPlugin = require('circular-dependency-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const TsDeclarationBundlerPlugin = require("ts-declaration-webpack-plugin");
+const CircularDependencyPlugin = require("circular-dependency-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 // const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
-const NullPlugin = require('webpack-null-plugin');
-const path = require('path');
-const webpack = require('webpack');
-const pkg = require('./package.json');
+const NullPlugin = require("webpack-null-plugin");
+const path = require("path");
+const webpack = require("webpack");
+const pkg = require("./package.json");
 
-let mode = process.env.NODE_ENV || 'development';
+let mode = process.env.NODE_ENV || "development";
 
 let MinifyPlugin = null;
-if (mode === 'production') {
+if (mode === "production") {
 	MinifyPlugin = require("babel-minify-webpack-plugin");
 }
 
 const banner = new webpack.BannerPlugin({
 	banner:
-		'Gadgets v' + pkg.version + '\n' +
+		"Gadgets v" +
+		pkg.version +
+		"\n" +
 		`Mode: ${mode}\n` +
-		'https://www.npmjs.com/package/gadgets\n' +
-		'Copyright (c) 2018, James Quigley\n',
+		"https://www.npmjs.com/package/gadgets\n" +
+		"Copyright (c) 2018, James Quigley\n",
 	entryOnly: true
 });
 
@@ -40,36 +42,77 @@ module.exports = {
 		minimize: false
 	},
 	entry: [
-		path.resolve(__dirname, 'node_modules', 'font-awesome', 'css', 'font-awesome.min.css'),
-		path.resolve(__dirname, 'node_modules', 'css-ripple-effect', 'dist', 'ripple.min.css'),
-		path.resolve(__dirname, 'node_modules', 'quill', 'dist', 'quill.core.css'),
-		path.resolve(__dirname, 'node_modules', 'quill', 'dist', 'quill.bubble.css'),
-		path.resolve(__dirname, 'node_modules', 'quill', 'dist', 'quill.snow.css'),
-		path.resolve(__dirname, 'node_modules', 'quill-markup', 'public', 'styles.css'),
-		path.resolve(__dirname, 'node_modules', 'react-sortable-tree', 'style.css'),
-		path.resolve(__dirname, 'index.ts')
+		path.resolve(
+			__dirname,
+			"node_modules",
+			"font-awesome",
+			"css",
+			"font-awesome.min.css"
+		),
+		path.resolve(
+			__dirname,
+			"node_modules",
+			"css-ripple-effect",
+			"dist",
+			"ripple.min.css"
+		),
+		path.resolve(
+			__dirname,
+			"node_modules",
+			"quill",
+			"dist",
+			"quill.core.css"
+		),
+		path.resolve(
+			__dirname,
+			"node_modules",
+			"quill",
+			"dist",
+			"quill.bubble.css"
+		),
+		path.resolve(
+			__dirname,
+			"node_modules",
+			"quill",
+			"dist",
+			"quill.snow.css"
+		),
+		path.resolve(
+			__dirname,
+			"node_modules",
+			"quill-markup",
+			"public",
+			"styles.css"
+		),
+		path.resolve(
+			__dirname,
+			"node_modules",
+			"react-sortable-tree",
+			"style.css"
+		),
+		path.resolve(__dirname, "index.ts")
 	],
-	target: 'node',
+	target: "node",
 	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js',
+		path: path.resolve(__dirname, "dist"),
+		filename: "bundle.js",
 		libraryTarget: "umd"
 	},
 	resolve: {
-		extensions: ['.ts', '.tsx', '.js', '.jsx', '.css']
+		extensions: [".ts", ".tsx", ".js", ".jsx", ".css"]
 	},
 	externals: {
 		react: {
-			root: 'React',
-			commonjs2: 'react',
-			commonjs: 'react',
-			amd: 'react'
+			root: "React",
+			commonjs2: "react",
+			commonjs: "react",
+			amd: "react"
 		},
-		'react-dom': {
-			root: 'ReactDOM',
-			commonjs2: 'react-dom',
-			commonjs: 'react-dom',
-			amd: 'react-dom'
+		"react-dom": {
+			root: "ReactDOM",
+			commonjs2: "react-dom",
+			commonjs: "react-dom",
+			amd: "react-dom"
 		}
 	},
 	resolveLoader: {
@@ -80,7 +123,7 @@ module.exports = {
 			{
 				test: /\.tsx?$/,
 				exclude: /node_modules|dist|demo|.*\.test\.tsx/,
-				loader: 'js-output-loader!ts-loader'
+				loader: "js-output-loader!ts-loader"
 			},
 			{
 				test: /\.css$/,
@@ -90,10 +133,10 @@ module.exports = {
 						loader: "css-loader",
 						options: {
 							importLoaders: 1,
-							localIdentName: '[folder]_[local]-[hash:base64:5]'
+							localIdentName: "[folder]_[local]-[hash:base64:5]"
 						}
 					},
-					'postcss-loader'
+					"postcss-loader"
 				]
 			},
 			{
@@ -106,7 +149,8 @@ module.exports = {
 			},
 			{
 				test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-				loader: "url-loader?limit=10000&mimetype=application/octet-stream"
+				loader:
+					"url-loader?limit=10000&mimetype=application/octet-stream"
 			},
 			{
 				test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
@@ -127,17 +171,19 @@ module.exports = {
 		}),
 		new MiniCssExtractPlugin({filename: "styles.css"}),
 		new webpack.optimize.ModuleConcatenationPlugin(),
-		new CopyWebpackPlugin([{
-			from: 'node_modules/quill-markup/public/highlights/**/*.css',
-			to: 'highlights',
-			flatten: true
-		}]),
+		new CopyWebpackPlugin([
+			{
+				from: "node_modules/quill-markup/public/highlights/**/*.css",
+				to: "highlights",
+				flatten: true
+			}
+		]),
 		MinifyPlugin ? new MinifyPlugin() : new NullPlugin(),
 		new webpack.SourceMapDevToolPlugin({
-			filename: '[name].js.map'
+			filename: "[name].js.map"
 		}),
 		new TsDeclarationBundlerPlugin({
-			name: 'bundle.d.ts'
+			name: "bundle.d.ts"
 		})
 		// new BundleAnalyzerPlugin(),
 	]
